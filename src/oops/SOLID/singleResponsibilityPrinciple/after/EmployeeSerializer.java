482c46a7321ba09549a9f9f32cacf6d2354470a3
srathi23 <shubhamrathi@Shubhams-MacBook-Air.local>
2024-09-13 23:05:48 +0530

    add | code change to resolve single responsibility principle violations

---
+++ src/oops/SOLID/singleResponsibilityPrinciple/after/EmployeeSerializer.java
@@ -0,0 +1,23 @@
+package oops.SOLID.singleResponsibilityPrinciple.after;
+
+public class EmployeeSerializer {
+
+    public String serializeEmployee(Employee employee) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("### EMPLOYEE RECORD ####");
+        sb.append(System.lineSeparator());
+        sb.append("NAME: ");
+        sb.append(employee.getFullName());
+        sb.append(System.lineSeparator());
+        sb.append("POSITION: ");
+        sb.append(employee.getClass().getTypeName());
+        sb.append(System.lineSeparator());
+        sb.append("EMAIL: ");
+        sb.append(employee.getEmail());
+        sb.append(System.lineSeparator());
+        sb.append("MONTHLY WAGE: ");
+        sb.append(employee.getMonthlyIncome());
+        sb.append(System.lineSeparator());
+        return sb.toString();
+    }
+}
